633182bd1e239717cfc9277fff4b62a511b18e48
Ber468 <bernardoradin.dalpozzo@gmail.com>
2024-03-06 13:56:58 -0300

    Implementacao sobre testes de integracao no sistema

--- src/main/java/com/example/swplanetapi/domain/Planet.java
+++ src/main/java/com/example/swplanetapi/domain/Planet.java
@@ -1,10 +1,12 @@
 package com.example.swplanetapi.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
 
@@ -15,8 +17,16 @@ public class Planet {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty
+    @Column(nullable = false, unique = true)
     private String name;
+
+    @NotEmpty
+    @Column(nullable = false)
     private String climate;
+
+    @NotEmpty
+    @Column(nullable = false)
     private String terrain;
 
     public Planet() {
@@ -28,6 +38,13 @@ public class Planet {
     }
 
     public Planet(String name, String climate, String terrain) {
+        this.name = name;
+        this.climate = climate;
+        this.terrain = terrain;
+    }
+
+    public Planet(Long id, String name, String climate, String terrain) {
+        this.id = id;
         this.name = name;
         this.climate = climate;
         this.terrain = terrain;
@@ -69,4 +86,10 @@ public class Planet {
     public boolean equals(Object obj) {
         return EqualsBuilder.reflectionEquals(obj, this);
     }
+
+    @Override
+    public String toString() {
+        return "Planet [climate=" + climate + ", id=" + id + ", name=" + name + ", terrain=" + terrain + "]";
+    }
+
 }
